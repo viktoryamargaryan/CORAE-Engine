b22e5f5e5608712c1fa20a78cdcd0431ce0d5a7d
viktoryamargaryan <153457659+viktoryamargaryan@users.noreply.github.com>
2026-04-21 18:55:09 +0400

    structures.py

--- structures.py
+++ structures.py
@@ -9,3 +9,19 @@ class Job:
     def __init__(self, job_id, required_capacity):
         self.job_id = job_id
         self.required_capacity = required_capacity
+
+class AVLNode:
+    def __init__(self, machine):
+        self.machine = machine
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def insert(self, root, machine):
+        # Այստեղ կգրես AVL-ի ստանդարտ ինսերտը
+        pass
+
+    def find_best_fit(self, root, required_capacity):
+        # Սա քո ամենակարևոր ֆունկցիան է լինելու
+        pass
